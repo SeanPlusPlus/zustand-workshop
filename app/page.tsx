25ee9563a6a192322f58a528234fa23b0a9ee6c6
SeanPlusPlus <seanmichaelstephenson@gmail.com>
2025-07-22 12:48:34 -0700

    zus initial

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,38 @@
+'use client'
+
+import { create } from 'zustand'
+
+type BearState = {
+  bears: number
+  increasePopulation: () => void
+  removeAllBears: () => void
+  updateBears: (newBears: number) => void
+}
+
+const useStore = create<BearState>((set) => ({
+  bears: 0,
+  increasePopulation: () => set((state) => ({ bears: state.bears + 1 })),
+  removeAllBears: () => set({ bears: 0 }),
+  updateBears: (newBears: number) => set({ bears: newBears }),
+}))
+
+function BearCounter() {
+  const bears = useStore((state) => state.bears)
+  return <h1>{bears} bears around here...</h1>
+}
+
+function Controls() {
+  const increasePopulation = useStore((state) => state.increasePopulation)
+  return <button onClick={increasePopulation}>one up</button>
+}
+
 export default function Home() {
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <h1 className="text-4xl font-bold text-center">Hello World</h1>
+        <BearCounter />
+        <Controls />
       </main>
     </div>
   )
